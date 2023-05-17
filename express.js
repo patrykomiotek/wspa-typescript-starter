9bc09a61f6b810598c292e97f40a11aeee2f7afd
Patryk Omiotek <patryk@android-473b0de2f8494ba0.wspa.edu.pl>
2023-05-17 15:51:16 +0200

    feat: body parser for body post requests

--- express.js
+++ express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/products/:id', (req, res, next) => {
   res.json({msg: 'This is CORS-enabled for all origins!'})
@@ -13,10 +15,22 @@ app.get('/users', (req, res) => {
   res.json({ firstName: 'John', lastName: 'Smith', age: 55 });
 });
 
+app.post('/users', (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+    res.json({ ok: true });
+});
+
+app.get('/ping', (req, res) => {
+    res.status(200);
+    res.json({ status: 'ok' });
+});
+
 app.get('/users/1', (req, res) => {
     res.status(200);
     res.json({ firstName: 'Eve' });
-  });
+});
 
 app.listen(8000, () => {
   console.log('CORS-enabled web server listening on port 8000');
